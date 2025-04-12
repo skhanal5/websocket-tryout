ecc70ba9627bd3b3cbbcde1001a4fd525c12bb01
Subodh <skhanal5@terpmail.umd.edu>
2025-04-12 15:29:36 -0400

    Add shadcn form and dialog dependencies. Define RegisterPopup component that allows user to enter a username

--- websocket-ui/src/app/App.tsx
+++ websocket-ui/src/app/App.tsx
@@ -1,28 +1,43 @@
 import Container from "@/components/containers/Container";
 import Chat from "@/features/conversation/Conversation";
+import RegisterPopup from "@/features/register/Register";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    setUser(null)
+  }, []);
+
   return (
     <Container className="flex-1 bg-slate-100">
-      <Chat contact={{
-        profilePicture: "",
-        name: "Subodh Khanal"
-      }} messages={[
-        {
-          id: "",
-          message: "hello!",
-          sender: "me",
-          timestamp: "7:27 PM",// should be a date
-          profilePicture: ""
-        },
-        {
-          id: "",
-          message: "hi back!",
-          sender: "other",
-          timestamp: "7:27 PM",// should be a date
-          profilePicture: ""
-        },
-      ]}/>
+      {user !== null ? (
+        <Chat
+          contact={{
+            profilePicture: "",
+            name: "Subodh Khanal",
+          }}
+          messages={[
+            {
+              id: "",
+              message: "hello!",
+              sender: "me",
+              timestamp: "7:27 PM", // should be a date
+              profilePicture: "",
+            },
+            {
+              id: "",
+              message: "hi back!",
+              sender: "other",
+              timestamp: "7:27 PM", // should be a date
+              profilePicture: "",
+            },
+          ]}
+        />
+      ) : (
+        <RegisterPopup/>
+      )}
     </Container>
-  )
+  );
 }
---
+++ websocket-ui/src/features/register/Register.tsx
@@ -0,0 +1,77 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+export default function RegisterPopup() {
+  const [open, setOpen] = useState(true);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    localStorage.setItem("user", values.username);
+    setOpen(false)
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent onInteractOutside={(e) => e.preventDefault()}>
+        <DialogHeader>
+          <DialogTitle>Enter your username</DialogTitle>
+          <DialogDescription>
+            Set your username and start chatting with other users!
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="hello-world1998" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="hover:cursor-pointer" type="submit">
+              Submit
+            </Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
